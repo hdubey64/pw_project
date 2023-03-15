794d687aadb39fcb3924c3bd4aa37db9ab167e34
= <=>
2023-03-15 16:41:09 +0530

    Expression Function Done

---
+++ JavaScript/002_javaScript_FundaMental/005_expressionFunction.js
@@ -0,0 +1,20 @@
+let x = function exex() {
+   console.log("Hi");
+};
+
+x();
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+const add = function (a, b) {
+   return a - b;
+};
+
+console.log(add(10, 20));
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//Expression Function ko arrow function  ki roop me likhna
+const add2 = (a, b) => {
+   return a + b;
+};
+
+console.log(add2(10, 20));
