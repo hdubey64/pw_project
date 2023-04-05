b8e782747879b254c950024b5ab1a2766cfc6a4a
= <=>
2023-04-05 16:53:25 +0530

    Password Validation Form

--- JavaScript/001_MileStone_Assingement/Pssword_Validation/script.js
+++ JavaScript/001_MileStone_Assingement/Pssword_Validation/script.js
@@ -2,15 +2,19 @@ document.querySelector("#demoForm").addEventListener("submit", (event) => {
    event.preventDefault();
    let emailInput = document.querySelector("#mail");
    let passwordInput = document.querySelector("#passCode");
-
    let email = emailInput.value;
    let password = passwordInput.value;
+   let massage = document.querySelector("#para");
 
-   if (email.includes("@") && password.length === 8) {
-      document.querySelector("#para").innerText = "Login successful!";
+   if (email.includes("@") && password.length >= 8) {
+      massage.innerText = "🙂  Login successful....! 👍👍👍 ";
+      massage.style.color = "green";
+      alert(`🙏 Welcome ${email} ! 🙂 `);
    } else {
-      document.querySelector("#para").innerText = "Invalid email or password.";
+      massage.innerText = "😔  Incorrect email or password !!!";
+      massage.style.color = "red";
+      alert(
+         `Opps.....! Please check your email or password is incorrect ! 😔 `
+      );
    }
-   console.log(emailInput.value);
-   console.log(passwordInput.value);
 });
