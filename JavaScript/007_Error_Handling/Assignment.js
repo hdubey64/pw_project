b3d7dd2826ea87a6b138be4d2760e98bcc972215
Vishal Dubey <hdubey64@gmail.com>
2023-05-10 22:50:28 +0530

    Working On Assignment

---
+++ JavaScript/007_Error_Handling/Assignment.js
@@ -0,0 +1,24 @@
+// // Q. 1
+// const convertToNumber = (string) => {
+//    try {
+//       let result = parseInt(string);
+
+//       if (result.toString() === "NaN") {
+//          throw new Error("err");
+//       }
+//       return result;
+//    } catch (error) {
+//       return "Inavlid Number";
+//    }
+// };
+
+// let string = "abcd";
+
+// console.log(convertToNumber(string));
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//Q.2
+
+const getPerson = (person) => {
+   //
+};
