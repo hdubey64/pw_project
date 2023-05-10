f287d79de0092536b9619bda2b1b026e23aadb30
Vishal Dubey <hdubey64@gmail.com>
2023-05-11 01:44:53 +0530

    Rivision Classes 27March

---
+++ JavaScript/001_Revison_Class/002_27March.js
@@ -0,0 +1,26 @@
+// let star = 4;
+// if (star == 5) {
+//    console.log("Excellent Service");
+// } else if (star == 3) {
+//    console.log("good Service");
+// } else {
+//    console.log("Averge Service");
+// }
+
+// for (i = 0; i < 5; i++) {
+//    console.log(i);
+// }
+
+for (i = 0; i < 5; i++) {
+   if (i == 3) {
+      break;
+   }
+   console.log(i);
+}
+console.log("+++++++++++++++++++++++");
+for (i = 0; i < 5; i++) {
+   if (i == 3) {
+      continue;
+   }
+   console.log(i);
+}
