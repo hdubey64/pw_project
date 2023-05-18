b007433f2a96b7320f0b3be83996bb2b87a26fcb
Vishal Dubey <hdubey64@gmail.com>
2023-05-18 11:24:19 +0530

    Live Class Done

---
+++ JavaScript/001_Revison_Class/003_31March.js
@@ -0,0 +1,9 @@
+let names = ["Vishal", "Shashank", "ashutosh"];
+
+console.log(names);
+console.log("The length of an array nemes is:", names.length);
+console.log(names[names.length - 1]);
+console.log(names[2]);
+
+names.push("Raj");
+console.log(names);
