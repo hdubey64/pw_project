726bf760a6beb34386ff822e82d800e944fec2f7
Vishal Dubey <hdubey64@gmail.com>
2023-05-19 16:57:42 +0530

    03 April Reivsion Class

---
+++ JavaScript/001_Revison_Class/004_03April.js
@@ -0,0 +1,19 @@
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// console.log(arr.pop());
+// console.log(arr);
+// console.log(arr.at(5));
+
+//shift and Unshift
+
+// console.log(arr.shift());
+// console.log(arr.unshift(10));
+
+// console.log(arr.slice(1, 3));
+// console.log(arr.splice(1, 3));
+// let newArr = arr.splice(1, 3, "Vishal", "Shashank");
+
+// console.log(newArr);
+
+console.log(arr.reverse());
+console.log(arr.sort());
+console.log(arr);
