62ca2cde788addf4cc16dd217bcb83e2b4d1ca58
Vishal Dubey <hdubey64@gmail.com>
2023-07-12 17:36:49 +0530

    Folder Setup

--- Node.js/Express.js/crudApp/app.js
+++ Node.js/Express.js/crudApp/app.js
@@ -1,5 +1,21 @@
+require("dotenv").config();
 const express = require("express");
-
 const app = express();
+const cors = require("cors");
 
-export default app;
+const connectToDB = require("./config/db.js");
+
+//Express middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
+
+//init connection to db
+connectToDB();
+
+const userRoutes = require("./routes/userRoutes.js");
+
+app.use("/", userRoutes);
+
+module.exports = app;
