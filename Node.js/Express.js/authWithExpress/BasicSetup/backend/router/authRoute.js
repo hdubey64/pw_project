daa4bdd8328513e55d0f5a913182f95865e05b01
Vishal Dubey <hdubey64@gmail.com>
2023-07-16 13:57:19 +0530

    Logout Router

--- Node.js/Express.js/authWithExpress/BasicSetup/backend/router/authRoute.js
+++ Node.js/Express.js/authWithExpress/BasicSetup/backend/router/authRoute.js
@@ -1,10 +1,16 @@
 const express = require("express");
-const { signup, signIn, getUser } = require("../controllers/authControllers");
+const {
+   signup,
+   signIn,
+   getUser,
+   logout,
+} = require("../controllers/authControllers");
 const jwtAuth = require("../middleware/jwtAuth");
 const authRouter = express.Router();
 
 authRouter.post("/signup", signup);
 authRouter.post("/signin", signIn);
 authRouter.get("/user", jwtAuth, getUser);
+authRouter.get("/logout", jwtAuth, logout);
 
 module.exports = authRouter;
