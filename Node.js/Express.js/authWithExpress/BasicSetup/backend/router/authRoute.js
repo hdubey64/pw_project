bc402a3f194524af1d4cd67560a2a5ca270bf8c2
Vishal Dubey <hdubey64@gmail.com>
2023-07-16 13:20:07 +0530

    Signin and getUser Router

--- Node.js/Express.js/authWithExpress/BasicSetup/backend/router/authRoute.js
+++ Node.js/Express.js/authWithExpress/BasicSetup/backend/router/authRoute.js
@@ -1,7 +1,10 @@
 const express = require("express");
-const { signup } = require("../controllers/authControllers");
+const { signup, signIn, getUser } = require("../controllers/authControllers");
+const jwtAuth = require("../middleware/jwtAuth");
 const authRouter = express.Router();
 
 authRouter.post("/signup", signup);
+authRouter.post("/signin", signIn);
+authRouter.get("/user", jwtAuth, getUser);
 
 module.exports = authRouter;
