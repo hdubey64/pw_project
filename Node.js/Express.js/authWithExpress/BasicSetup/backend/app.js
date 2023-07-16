7acb98629b5235a82fb1ed581166fad201dc5780
Vishal Dubey <hdubey64@gmail.com>
2023-07-16 13:20:41 +0530

    Cooksie paersor import and Use

--- Node.js/Express.js/authWithExpress/BasicSetup/backend/app.js
+++ Node.js/Express.js/authWithExpress/BasicSetup/backend/app.js
@@ -2,10 +2,13 @@ const express = require("express");
 const app = express();
 const authRouter = require("./router/authRoute");
 const databaseConnect = require("./config/databseConfig");
+const cookieParser = require("cookie-parser");
 
 databaseConnect();
 
 app.use(express.json());
+app.use(cookieParser());
+
 app.use("/api/auth/", authRouter);
 
 app.use("/", (req, res) => {
