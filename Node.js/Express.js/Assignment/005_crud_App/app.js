07251c2020cb6a8d52480bb0c506b2ae4219a293
Vishal Dubey <hdubey64@gmail.com>
2023-07-17 17:03:14 +0530

    Router imported and used

--- Node.js/Express.js/Assignment/005_crud_App/app.js
+++ Node.js/Express.js/Assignment/005_crud_App/app.js
@@ -7,7 +7,14 @@ const express = require("express");
 // Create an instance of the Express application
 const app = express();
 
+const router = require("./router/routes");
+
+// const connectToDB = require("./config/databaseConfig");
+
+// connectToDB();
+
 // Enable parsing of JSON data in the request body
 app.use(express.json());
+app.use("/", router);
 
 module.exports = app;
