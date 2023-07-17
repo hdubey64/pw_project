4420676502dba9093125db8228cf12fc6b17bbbf
Vishal Dubey <hdubey64@gmail.com>
2023-07-17 18:56:32 +0530

    UserLogIn Done

--- Node.js/Express.js/Assignment/005_crud_App/router/routes.js
+++ Node.js/Express.js/Assignment/005_crud_App/router/routes.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const { userRegistration } = require("../controller/userController");
+const { userRegistration, userLogin } = require("../controller/userController");
 
 const router = express.Router();
 router.post("/ragister", userRegistration);
+router.post("/login", userLogin);
 
 module.exports = router;
