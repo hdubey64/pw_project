845be893719fa5e08678970b9fd83578ce7d71bf
Vishal Dubey <hdubey64@gmail.com>
2023-07-17 11:50:47 +0530

    Importing cors and used

--- Node.js/Express.js/authWithExpress/BasicSetup/backend/app.js
+++ Node.js/Express.js/authWithExpress/BasicSetup/backend/app.js
@@ -3,11 +3,17 @@ const app = express();
 const authRouter = require("./router/authRoute");
 const databaseConnect = require("./config/databseConfig");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 databaseConnect();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+   cors({
+      origin: [process.env.CLIENT_URL],
+   })
+);
 
 app.use("/api/auth/", authRouter);
 
