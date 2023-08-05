90fa1df5f52afa16d06161de0388b5f5a1b3e534
Vishal Dubey <hdubey64@gmail.com>
2023-08-05 19:36:49 +0530

    UseStaet and Hoioks

---
+++ ReactJs/ReactPartTwo/001_useState_and_Hooks/my-app/src/components/useState_And_Hooks/index.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+let z = 0;
+const Counter = () => {
+   const [x, setX] = useState(0);
+   const [y, setY] = useState(0);
+
+   // useEffect(() => {
+   //    document.title = `Count: ${y}`;
+   // }, [y]);
+   return (
+      <div>
+         count: {z}
+         <button onClick={() => (z += 1)}>Icreament</button>
+         <br />
+         count: {x}
+         <button onClick={() => setX(x + 1)}>Icreament</button>
+         <button onClick={() => setX(x - 1)}>Decrement</button>
+         <br />
+         count: {y}
+         <button onClick={() => setY(y + 1)}>Icreament</button>
+         <button onClick={() => setY(y - 1)}>Decrement</button>
+      </div>
+   );
+};
+
+export default Counter;
