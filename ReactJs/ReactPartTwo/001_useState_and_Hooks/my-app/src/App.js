568364baea60aa49d4d92f2b9252943dbfd5cdcb
Vishal Dubey <hdubey64@gmail.com>
2023-08-05 19:37:24 +0530

    NEw Folder Setup

--- ReactJs/ReactPartTwo/001_useState_and_Hooks/my-app/src/App.js
+++ ReactJs/ReactPartTwo/001_useState_and_Hooks/my-app/src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Counter from "./components";
+import Counter from "./components/useState_And_Hooks";
+import ConditionalRendring from "./components/useState_And_Hooks/002_index";
 
 function App() {
    return (
       <div className="App">
          <Counter />
+         <ConditionalRendring />
       </div>
    );
 }
