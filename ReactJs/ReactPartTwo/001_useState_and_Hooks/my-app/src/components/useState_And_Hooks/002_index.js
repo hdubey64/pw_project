9d136f7462586ff1e7859e565fb9a8e63b1cc7d2
Vishal Dubey <hdubey64@gmail.com>
2023-08-07 16:34:30 +0530

    Conditional Rendring

--- ReactJs/ReactPartTwo/001_useState_and_Hooks/my-app/src/components/useState_And_Hooks/002_index.js
+++ ReactJs/ReactPartTwo/001_useState_and_Hooks/my-app/src/components/useState_And_Hooks/002_index.js
@@ -1,12 +1,20 @@
 import React, { useState, useEffect } from "react";
 let z = 0;
-const ConditionalRendring = () => {
+const ConditionalRendring = ({ title }) => {
    const [x, setX] = useState(0);
    const [isEdtting, setIseditting] = useState(false);
    const [todos, setTodos] = useState(["todo1", "todo2"]);
+   const [isLoggedIn, setIsLoggedIn] = useState(true);
+   let user;
+   if (isLoggedIn) {
+      user = "You are logged in";
+   } else {
+      user = "Please Login";
+   }
 
    return (
       <div>
+         <h1>{title}</h1>
          count: {x} is {x % 2 == 0 ? "Even" : "Odd"}
          <br />
          <button onClick={() => setX(x + 1)}>Icreament</button>
@@ -23,6 +31,12 @@ const ConditionalRendring = () => {
                Click
             </button>
          </ul>
+         <br />
+         {user}
+         <br />
+         <button onClick={() => setIsLoggedIn(!isLoggedIn)}>
+            {isLoggedIn ? "Logout" : "Login"}
+         </button>
       </div>
    );
 };
