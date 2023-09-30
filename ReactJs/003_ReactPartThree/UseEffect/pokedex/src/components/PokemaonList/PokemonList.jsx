7bd0881204b065dcc79e85cc1b456a4c8050d14a
Vishal Dubey <hdubey64@gmail.com>
2023-09-30 17:32:51 +0530

    Pokemaon h1 Alignment

--- ReactJs/003_ReactPartThree/UseEffect/pokedex/src/components/PokemaonList/PokemonList.jsx
+++ ReactJs/003_ReactPartThree/UseEffect/pokedex/src/components/PokemaonList/PokemonList.jsx
@@ -1,50 +1,65 @@
-import React, { useEffect, useState } from 'react'
-import axios from "axios"
-import "./PokemonList.css"
-import Pokenmon from "../Pokemon/Pokemon"
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./PokemonList.css";
+import Pokenmon from "../Pokemon/Pokemon";
 
-const POKEDEX_URL = 'https://pokeapi.co/api/v2/pokemon'
+const POKEDEX_URL = "https://pokeapi.co/api/v2/pokemon";
 
 const PokemonList = () => {
+   const [pokemonList, setPokemonList] = useState([]);
+   const [isLoading, setIsLoading] = useState(true);
+   async function downloadPkemon() {
+      //Pokemon Url Download list of 20 Pokemon
+      const response = await axios.get(POKEDEX_URL);
+      const pokemonResult = response.data.results; // We get array of pokemons from results
 
+      //Iterating ober the array of pokomons, and using their url, to create an array of promices
+      // that will download those 20 pokemons
+      const pokemonResultPromise = pokemonResult.map((pokemon) =>
+         axios.get(pokemon.url)
+      );
 
-    const [pokemonList, setPokemonList] = useState([])
-    const [isLoading, setIsLoading] = useState(true)
-    async function downloadPkemon() {
-        //Pokemon Url Download list of 20 Pokemon
-        const response = await axios.get(POKEDEX_URL)
+      //Passing that promise array to axios.all
+      const pokemonData = await axios.all(pokemonResultPromise); // array of 20 pokemaon detailed data
+      console.log(pokemonData);
 
-        const pokemonResult = response.data.results // We get array of pokemons from results
+      //now iterate on the data of each pokeon,and extract id, name, image, types
+      const res = pokemonData.map((pokedata) => {
+         const pokemon = pokedata.data;
+         return {
+            id: pokemon.id,
+            name: pokemon.name,
+            image: pokemon.sprites.other
+               ? pokemon.sprites.other.dream_world.front_default
+               : pokemon.sprites,
+            types: pokemon.types,
+         };
+      });
+      console.log(res);
+      setPokemonList(res);
+      setIsLoading(false);
+   }
 
-        //Iterating ober the array of pokomons, and using their url, to create a 
-        const pokemonResultPromise = pokemonResult.map((pokemon) => axios.get(pokemon.url))
-        const pokemonData = await axios.all(pokemonResultPromise)
-        console.log(pokemonData)
-        const res = pokemonData.map((pokedata) => {
-            const pokemon = pokedata.data
-            return {
-                id: pokemon.id,
-                name: pokemon.name,
-                image: (pokemon.sprites.other) ? pokemon.sprites.other.dream_world.front_default : pokemon.sprites,
-                types: pokemon.types
-            }
-        })
-        console.log(res)
-        setPokemonList(res)
-        setIsLoading(false)
-    }
+   useEffect(() => {
+      downloadPkemon();
+   }, []);
 
-    useEffect(() => {
-        downloadPkemon()
+   return (
+      <div className="wrapper">
+         <h1> Pokemon List</h1>
+         <div className="pokemonListWrapper">
+            {isLoading
+               ? "Loading......."
+               : pokemonList.map((p) => (
+                    <Pokenmon key={p.id} name={p.name} image={p.image} />
+                 ))}
+         </div>
+         <div className="control">
+            <button>Pre</button>
+            <button>Next</button>
+         </div>
+      </div>
+   );
+};
 
-    }, [])
-
-    return (
-        <div className='pokemonListWrapper'>
-            Pokemon List
-            {(isLoading) ? 'Loading.......' : pokemonList.map((p) => <Pokenmon key={p.id} name={p.name} image={p.image} />)}
-        </div>
-    )
-}
-
-export default PokemonList
+export default PokemonList;
