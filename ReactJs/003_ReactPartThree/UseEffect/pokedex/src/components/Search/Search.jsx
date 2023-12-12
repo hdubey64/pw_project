3817cc4a438003ef202c75c340f61b0a2a9f8d62
Vishal Dubey <hdubey64@gmail.com>
2023-12-12 12:42:24 +0530

    Debouncing Implement

--- ReactJs/003_ReactPartThree/UseEffect/pokedex/src/components/Search/Search.jsx
+++ ReactJs/003_ReactPartThree/UseEffect/pokedex/src/components/Search/Search.jsx
@@ -1,13 +1,15 @@
+import useDebounce from "../../hooks/useDebounce";
 import "./Search.css";
 
 const Search = ({ updateSerchTerm }) => {
+   const debounceCallBack = useDebounce((e) => updateSerchTerm(e.target.value));
    return (
       <div className="searchWrapper">
          <input
             type="text"
             id="pkemonNameSearch"
             placeholder="Pokemon Name"
-            onChange={(e) => updateSerchTerm(e.target.value)}
+            onChange={debounceCallBack}
          />
       </div>
    );
